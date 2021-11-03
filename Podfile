dbab50c7d7035334e487995b7727a1b017d1c7ca
peggychiang88 <peggychiang08@gmail.com>
2021-11-03 13:00:23 -0500

    Added Firebase

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '9.0'
+
+target 'UTSwap' do
+  use_frameworks!
+
+  # Pods for UTSwap
+# add the Firebase pod for Google Analytics
+pod 'Firebase/Analytics'
+pod 'Firebase/Auth'
+# or pod ‘Firebase/AnalyticsWithoutAdIdSupport’
+# for Analytics without IDFA collection capability
+end
---
+++ UTSwap/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  UTSwap
+//
+//  Created by Peggy Chiang on 11/3/21.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
