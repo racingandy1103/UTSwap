845e44c4132eabaae79885f01a301ea79d41d6ce
peggychiang88 <peggychiang08@gmail.com>
2021-11-08 11:37:26 -0600

    Login & Register, notifications update

--- UTSwap/ItemBuyViewController.swift
+++ UTSwap/ItemBuyViewController.swift
@@ -15,7 +15,30 @@ class ItemBuyViewController: BaseViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func buyPressed(_ sender: Any) {
+        
+        // create an object that holds the data for our notification
+        let notification = UNMutableNotificationContent()
+        notification.title = "Purchase Complete"
+        notification.subtitle = "Purchase Details"
+        notification.body = "Your purchase has succesfully completed!"
+        
+        // set up the notification's trigger
+        let notificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 30, repeats: false)
+        
+        // set up a request to tell iOS to submit the notification with that trigger
+        let request = UNNotificationRequest(identifier: "notification2",
+                                            content: notification,
+                                            trigger: notificationTrigger)
+        
+        
+        // submit the request to iOS
+        UNUserNotificationCenter.current().add(request) { (error) in
+            print("Request error: ",error as Any)
+        }
+        print("Submitted")
+    }
+    
     /*
     // MARK: - Navigation
 
