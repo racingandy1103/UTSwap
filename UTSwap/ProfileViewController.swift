06a7fe4428cb7763ef43006262e2e63cf1f138a4
racingandy <racingandy@utexas.edu>
2021-11-30 04:19:22 -0600

    Changes in Profile Paths

--- UTSwap/ProfileViewController.swift
+++ UTSwap/ProfileViewController.swift
@@ -13,15 +13,39 @@ class ProfileViewController: BaseViewController {
     
     var ref: DatabaseReference!
 
-
+    @IBOutlet weak var profilePic: UIImageView!
+    
     @IBOutlet weak var firstNameField: UITextField!
+    
     @IBOutlet weak var lastNameField: UITextField!
     @IBOutlet weak var addressField: UITextField!
+    
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var editProfileButton: UIButton!
+    
+    @IBOutlet weak var deleteAccountButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        UILabel.appearance().font = UIFont(name: "Courier", size: 15.0)
+        firstNameField.isEnabled = false
+        lastNameField.isEnabled = false
+        addressField.isEnabled = false
+        passwordField.isEnabled = false
+        
+        firstNameField.placeholder = "Enter New Name"
+        lastNameField.placeholder = "Enter New Name"
+        addressField.placeholder = "Enter new address"
+        passwordField.placeholder = "Enter new password"
         if (Auth.auth().currentUser != nil) {
             let user = Auth.auth().currentUser
 
@@ -33,9 +57,19 @@ class ProfileViewController: BaseViewController {
                 let lname = value?["lname"] as? String ?? ""
                 let address = value?["address"] as? String ?? ""
                 
-                self.addressField.text = address
-                self.firstNameField.text = fname
-                self.lastNameField.text = lname
+                self.addressLabel.text = address
+                self.firstNameLabel.text = fname
+                self.lastNameLabel.text = lname
+                
+                if (fname == ""){
+                    self.firstNameLabel.text = "No First Name Set"
+                }
+                if (lname == ""){
+                    self.lastNameLabel.text = "No Last Name Set"
+                }
+                if (address == ""){
+                    self.addressLabel.text = "No Address Set"
+                }
 
 
                 // ...
@@ -44,14 +78,24 @@ class ProfileViewController: BaseViewController {
               }
             }
         
+        
+        
     }
-    @IBAction func onSaveProfileInfo(_ sender: Any) {
+    
+    @IBAction func editProfileInfo(_ sender: Any) {
+        
+        firstNameField.isEnabled = true
+        lastNameField.isEnabled = true
+        addressField.isEnabled = true
+        passwordField.isEnabled = true
+        
         if (Auth.auth().currentUser != nil) {
             let user = Auth.auth().currentUser
             self.ref.child("users").child(user!.uid).setValue(["fname": self.firstNameField.text, "lname": self.lastNameField.text, "address": self.addressField.text])
         }
-        
     }
+    
+    
     
     override func viewWillAppear(_ animated: Bool) {
         self.showSettingsButton(show: false)
